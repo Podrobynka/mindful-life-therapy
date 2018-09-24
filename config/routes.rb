caf987e17499f610fe8e1f51c64e3379edfce61b
Stephen Murdoch <stephen@murdo.ch>
2018-09-24 01:52:08 +0100

    Temp routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get '/about', to: 'home_page#show', as: 'about'
+  get '/counselling-and-psychotherapy', to: 'home_page#show', as: 'counselling'
+  get '/mindfulness', to: 'home_page#show', as: 'mindfulness'
+  get '/contact', to: 'home_page#show', as: 'contact'
   root 'home_page#show'
 end
