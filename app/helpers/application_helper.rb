f80092c9a28564bfb523ab212cf1c536eac4afb1
Stephen Murdoch <stephen@murdo.ch>
2018-09-24 01:51:44 +0100

    Helper methods for active links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def css_class_for_controller(kontroller_name)
+    if request.path.match(kontroller_name) || (controller_name == kontroller_name)
+      'active'
+    end
+  end
+
+  def css_class_for_path(path)
+    if request.path.match(path)
+      'active'
+    end
+  end
+
+  def css_class_for_controller_and_action(kontroller_name, aktion_name)
+    css_class_for_controller(kontroller_name) if aktion_name == action_name
+  end
 end
