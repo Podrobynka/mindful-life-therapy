bd078855f45d810b2a70e2d813cfc2d03029c996
Stephen Murdoch <stephen@murdo.ch>
2018-10-01 04:28:55 +0100

    Tidy test

--- test/mailers/messages_mailer_test.rb
+++ test/mailers/messages_mailer_test.rb
@@ -1,16 +1,19 @@
 require 'test_helper'
 
 class MessagesMailerTest < ActionMailer::TestCase
+
   def setup
-    @message = Message.new name: 'awesome', email: 'awe@some.com', subject: 'awesome', body: 'awesome'
+    @message = Message.new name: 'zoe', email: 'zoe@abc.com', subject: 'hi', body: 'awesome sauce'
   end
 
   test "send_email" do
     mail = MessagesMailer.send_email(@message, 'info@mindfullifetherapy.co.uk')
 
-    assert_equal "awesome", mail.subject
     assert_equal ["info@mindfullifetherapy.co.uk"], mail.to
-    assert_equal ["awe@some.com"], mail.from
-    assert_match "awesome", mail.body.encoded
+    assert_equal ["zoe@abc.com"], mail.from
+    assert_equal "hi", mail.subject
+    assert_match "zoe", mail.body.encoded
+    assert_match "zoe@abc.com", mail.body.encoded
+    assert_match "awesome sauce", mail.body.encoded
   end
 end
