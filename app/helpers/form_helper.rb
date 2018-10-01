a85a0a255ef0a2e2bd179736ed24fb28b441d768
Stephen Murdoch <stephen@murdo.ch>
2018-10-01 04:32:24 +0100

    Format error messages better

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -2,4 +2,8 @@ module FormHelper
   def error_message target: 'errors.message'
     content_tag :div, nil, class: 'error-message hide', data: { target: target }
   end
+
+  def error_count_for target
+    pluralize target.errors.count, 'error'
+  end
 end
