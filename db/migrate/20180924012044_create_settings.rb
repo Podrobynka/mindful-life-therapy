153f394ece856ee62d121c2368c6a850490539d2
Stephen Murdoch <stephen@murdo.ch>
2018-10-01 04:29:27 +0100

    New fields

--- db/migrate/20180924012044_create_settings.rb
+++ db/migrate/20180924012044_create_settings.rb
@@ -3,8 +3,12 @@ class CreateSettings < ActiveRecord::Migration[5.2]
     create_table :settings do |t|
       t.string :telephone
       t.string :contact_email
-      t.string :office_address
-      t.string :hourly_rate
+      t.string :office_address_line_1
+      t.string :office_address_line_2
+      t.string :office_address_line_3
+      t.string :office_address_city
+      t.string :office_address_postcode
+      t.string :session_rate
 
       t.timestamps
     end
