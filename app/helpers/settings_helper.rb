7e2cffaa8d3269b91012cb235577c7a84c76c40c
Stephen Murdoch <stephen@murdo.ch>
2018-10-01 15:52:34 +0100

    Helper method for returning office address as a list

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -1,11 +1,33 @@
 module SettingsHelper
+
+  def office_address settings
+    content_tag :ul, class: 'no-bullet' do
+      company_name_as_list_item
+      address_details_as_list(settings)
+    end
+  end
+
   def footer_office_address settings
-    address_details = [
+    address_details(settings).join(', ')
+  end
+
+  def company_name_as_list_item
+    concat content_tag(:li, 'Mindful Life Therapy')
+  end
+
+  def address_details_as_list settings
+    address_details(settings).each do |detail|
+      concat content_tag :li, detail
+    end
+  end
+
+  def address_details settings
+    [
       settings[:office_address_line_1],
       settings[:office_address_line_2],
       settings[:office_address_line_3],
       settings[:office_address_city],
       settings[:office_address_postcode]
-    ].reject(&:blank?).join(', ')
+    ].reject(&:blank?)
   end
 end
--- test/helpers/settings_helper_test.rb
+++ test/helpers/settings_helper_test.rb
@@ -2,6 +2,44 @@ require 'test_helper'
 
 class SettingsHelperTest < ActionView::TestCase
 
+  test 'office_address returns a list with company name as first item' do
+    blank_fixture = {}
+    expected = "<ul class='no-bullet'><li>Mindful Life Therapy</li></ul>"
+    assert_dom_equal expected, office_address(blank_fixture)
+  end
+
+  test 'office_address extracts address details from provided argument' do
+    fixture = {
+      office_address_line_1: 'Awesome House',
+      office_address_line_2: 'Apartment 95',
+      office_address_line_3: 'Cool Street',
+      office_address_city: 'Amazing City',
+      office_address_postcode: 'G1 1AA'
+    }
+
+    expected = "<ul class='no-bullet'><li>Mindful Life Therapy</li><li>Awesome House</li><li>Apartment 95</li><li>Cool Street</li><li>Amazing City</li><li>G1 1AA</li></ul>"
+    assert_dom_equal expected, office_address(fixture)
+  end
+
+  test 'office_address rejects blank values from provided argument' do
+    fixture = {
+      office_address_line_1: 'Awesome House',
+      office_address_line_2: '',
+      office_address_line_3: nil,
+      office_address_city: 'Amazing City',
+      office_address_postcode: 'G1 1AA'
+    }
+
+    expected = "<ul class='no-bullet'><li>Mindful Life Therapy</li><li>Awesome House</li><li>Amazing City</li><li>G1 1AA</li></ul>"
+    assert_dom_equal expected, office_address(fixture)
+  end
+
+  # test 'office_address extracts address details from provided argument' do
+  #   fixture = { office_address_line_1: '123 Awesome Street' }
+  #   expected = "<ul class='no-bullet'><li>Mindful Life Therapy</li><li>123 Awesome Street</li></ul>"
+  #   assert_dom_equal expected, office_address(fixture)
+  # end
+
   test 'footer_office_address returns a string of comma separated values' do
     fixture = {
       office_address_line_1: 'Awesome House',
@@ -17,14 +55,14 @@ class SettingsHelperTest < ActionView::TestCase
 
   test 'footer_office_address rejects blank values' do
     fixture = {
-      office_address_line_1: '10 Newton Place',
+      office_address_line_1: 'Awesome House',
       office_address_line_2: '',
       office_address_line_3: nil,
-      office_address_city: 'Glasgow',
-      office_address_postcode: 'G7 9PR'
+      office_address_city: 'Amazing City',
+      office_address_postcode: 'G1 1AA'
     }
 
-    expected = '10 Newton Place, Glasgow, G7 9PR'
+    expected = 'Awesome House, Amazing City, G1 1AA'
     assert_equal expected, footer_office_address(fixture)
   end
 end
