702eec94e4c49cdb215e66b02bd6a759ecca5c5f
Stephen Murdoch <stephen@murdo.ch>
2018-10-01 04:30:47 +0100

    Remove comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :set_site_settings
 
   private
-
-  # Use callbacks to share common setup or constraints between actions.
+  
   def set_site_settings
     @settings = Setting.first
   end
