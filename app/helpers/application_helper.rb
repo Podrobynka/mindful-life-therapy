b843a9bbe1ea7dc2b1f69ea98d0435fb1ec6be4a
Stephen Murdoch <stephen@murdo.ch>
2018-10-01 04:31:45 +0100

    Move navbar related methods to nav_bar_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,2 @@
 module ApplicationHelper
-  def css_class_for_controller(kontroller_name)
-    if request.path.match(kontroller_name) || (controller_name == kontroller_name)
-      'active-link'
-    else
-      'inactive-link'
-    end
-  end
-
-  def css_class_for_path(path)
-    if request.path.match(path)
-      'active-link'
-    else
-      'inactive-link'
-    end
-  end
-
-  def css_class_for_controller_and_action(kontroller_name, aktion_name)
-    css_class_for_controller(kontroller_name) if aktion_name == action_name
-  end
 end
