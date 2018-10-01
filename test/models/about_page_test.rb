89e62a30adb22075b45b464324115ce85fea3d4e
Stephen Murdoch <stephen@murdo.ch>
2018-10-01 04:28:43 +0100

    Tighten and format model tests

--- test/models/about_page_test.rb
+++ test/models/about_page_test.rb
@@ -9,43 +9,37 @@ class AboutPageTest < ActiveSupport::TestCase
       meta_description: 'a' * 150
     }
 
-    about_page = AboutPage.new attrs
+    about_page = AboutPage.create attrs
     assert about_page.valid?
   end
 
   test 'title is required' do
-    about_page = AboutPage.new
-    refute about_page.valid?
+    about_page = AboutPage.create
     assert_match /blank/, about_page.errors[:title].to_s
   end
 
-  test 'meta_description is required' do
-    about_page = AboutPage.new
-    refute about_page.valid?
-    assert_match /blank/, about_page.errors[:meta_description].to_s
-  end
-
-  test 'body is required' do
-    about_page = AboutPage.new
-    refute about_page.valid?
-    assert_match /blank/, about_page.errors[:body].to_s
-  end
-
   test 'title has max length' do
-    about_page = AboutPage.new title: 'a' * 101
-    refute about_page.valid?
+    about_page = AboutPage.create title: 'a' * 101
     assert_match /too long/, about_page.errors[:title].to_s
   end
 
+  test 'meta_description is required' do
+    about_page = AboutPage.create
+    assert_match /blank/, about_page.errors[:meta_description].to_s
+  end
+
   test 'meta_description has max length' do
-    about_page = AboutPage.new meta_description: 'a' * 151
-    refute about_page.valid?
+    about_page = AboutPage.create meta_description: 'a' * 151
     assert_match /too long/, about_page.errors[:meta_description].to_s
   end
 
+  test 'body is required' do
+    about_page = AboutPage.create
+    assert_match /blank/, about_page.errors[:body].to_s
+  end
+
   test 'body has max length' do
-    about_page = AboutPage.new body: 'a' * 100001
-    refute about_page.valid?
+    about_page = AboutPage.create body: 'a' * 100001
     assert_match /too long/, about_page.errors[:body].to_s
   end
 end
--- test/models/setting_test.rb
+++ test/models/setting_test.rb
@@ -1,23 +1,100 @@
 require 'test_helper'
 
 class SettingTest < ActiveSupport::TestCase
-  setup do
-    @setting = Setting.create
+
+  test "creates a valid settings record" do
+    attrs = {
+      telephone: 'a' * 30,
+      contact_email: 'a' * 50,
+      office_address_line_1: 'a' * 50,
+      office_address_line_2: 'a' * 50,
+      office_address_line_3: 'a' * 50,
+      office_address_city: 'a' * 50,
+      office_address_postcode: 'a' * 50,
+      session_rate: 'a' * 50
+    }
+
+    setting = Setting.create attrs
+    assert setting.valid?
   end
 
   test "telephone is required" do
-    assert_match /blank/, @setting.errors[:telephone].to_s
+    setting = Setting.create
+    assert_match /blank/, setting.errors[:telephone].to_s
   end
 
-  test "office_address is required" do
-    assert_match /blank/, @setting.errors[:office_address].to_s
-  end
-
-  test "hourly_rate is required" do 
-    assert_match /blank/, @setting.errors[:hourly_rate].to_s
+  test "telephone has max length" do
+    setting = Setting.create telephone: '0' * 31
+    assert_match /too long/, setting.errors[:telephone].to_s
   end
 
   test "contact_email is required" do
-    assert_match /blank/, @setting.errors[:contact_email].to_s
+    setting = Setting.create
+    assert_match /blank/, setting.errors[:contact_email].to_s
+  end
+
+  test "contact_email has max length" do
+    setting = Setting.create contact_email: 'a' * 51
+    assert_match /too long/, setting.errors[:contact_email].to_s
+  end
+
+  test "office_address_line_1 is required" do
+    setting = Setting.create
+    assert_match /blank/, setting.errors[:office_address_line_1].to_s
+  end
+
+  test "office_address_line_1 has max length" do
+    setting = Setting.create office_address_line_1: 'a' * 51
+    assert_match /too long/, setting.errors[:office_address_line_1].to_s
+  end
+
+  test "office_address_line_2 is not required" do
+    setting = Setting.create
+    assert_empty setting.errors[:office_address_line_2]
+  end
+
+  test "office_address_line_2 has max length" do
+    setting = Setting.create office_address_line_2: 'a' * 51
+    assert_match /too long/, setting.errors[:office_address_line_2].to_s
+  end
+
+  test "office_address_line_3 is not required" do
+    setting = Setting.create
+    assert_empty setting.errors[:office_address_line_3]
+  end
+
+  test "office_address_line_3 has max length" do
+    setting = Setting.create office_address_line_3: 'a' * 51
+    assert_match /too long/, setting.errors[:office_address_line_3].to_s
+  end
+
+  test "office_address_city is required" do
+    setting = Setting.create
+    assert_match /blank/, setting.errors[:office_address_city].to_s
+  end
+
+  test "office_address_city has max length" do
+    setting = Setting.create office_address_city: 'a' * 51
+    assert_match /too long/, setting.errors[:office_address_city].to_s
+  end
+
+  test "office_address_postcode is required" do
+    setting = Setting.create
+    assert_match /blank/, setting.errors[:office_address_postcode].to_s
+  end
+
+  test "office_address_postcode has max length" do
+    setting = Setting.create office_address_postcode: 'a' * 51
+    assert_match /too long/, setting.errors[:office_address_postcode].to_s
+  end
+
+  test "session_rate is required" do
+    setting = Setting.create
+    assert_match /blank/, setting.errors[:session_rate].to_s
+  end
+
+  test "session_rate has max length" do
+    setting = Setting.create session_rate: 'a' * 51
+    assert_match /too long/, setting.errors[:session_rate].to_s
   end
 end
