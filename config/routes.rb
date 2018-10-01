3f60d8c11fd063e336713a76c3619bd20e0d9bf4
Stephen Murdoch <stephen@murdo.ch>
2018-10-01 04:28:26 +0100

    Hide unused routes for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resource :about_page, only: [:show, :edit, :update], controller: 'about_page'
+  resource :about_page, only: [:show, :edit, :update], controller: 'about_page', path: 'about'
   resource :settings, only: [:show, :edit, :update]
 
-  get '/about', to: 'about_page#show', as: 'about'
-  get '/counselling-and-psychotherapy', to: 'home_page#show', as: 'counselling'
-  get '/mindfulness', to: 'home_page#show', as: 'mindfulness'
+  # get '/about', to: 'about_page#show', as: 'about'
+  # get '/counselling-and-psychotherapy', to: 'home_page#show', as: 'counselling'
+  # get '/mindfulness', to: 'home_page#show', as: 'mindfulness'
   get '/contact', to: 'contact#new', as: 'contact'
   post '/contact', to: 'contact#create', as: 'create_contact'
 
