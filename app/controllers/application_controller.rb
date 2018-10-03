5045bbfc446dbc7162530500c4d51742fea3fdb8
Stephen Murdoch <stephen@murdo.ch>
2018-10-04 00:11:00 +0100

    White space

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :set_site_settings
 
   private
-  
-  def set_site_settings
-    @settings = Setting.first
-  end
+
+    def set_site_settings
+      @settings = Setting.first
+    end
 end
