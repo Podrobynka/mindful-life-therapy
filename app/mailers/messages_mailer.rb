48a19bc0630612443ecc431e9191d737734ca214
Stephen Murdoch <stephen@murdo.ch>
2018-10-03 18:47:47 +0100

    Deserialise message argument

--- app/mailers/messages_mailer.rb
+++ app/mailers/messages_mailer.rb
@@ -1,15 +1,18 @@
 class MessagesMailer < ApplicationMailer
 
   def send_email message, contact_email
-    @message = message
+    # incoming message will be json format
+    # because active job does not accept
+    # an unpersisted active model as an argument
+    @message = JSON.parse(message)
 
     mail(
       to: contact_email,
-      subject: message.subject,
-      from: message.email,
-      reply_to: message.email,
-      sender: message.email,
-      return_path: message.email
+      subject: @message['subject'],
+      from: @message['email'],
+      reply_to: @message['email'],
+      sender: @message['email'],
+      return_path: @message['email']
     )
   end
 end
