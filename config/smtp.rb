50ca23fd1d270c2deb80c7c3bc6d7bf88f8b2328
Stephen Murdoch <stephen@murdo.ch>
2018-10-03 01:48:41 +0100

    Update seeds file

--- config/smtp.rb
+++
@@ -1,9 +0,0 @@
-SMTP_SETTINGS = {
-  address: ENV.fetch('SMTP_ADDRESS'),
-  authentication: :plain,
-  domain: ENV.fetch('SMTP_DOMAIN'),
-  enable_starttls_auto: true,
-  password: ENV.fetch('SENDGRID_PASSWORD'),
-  port: '587',
-  user_name: ENV.fetch('SENDGRID_USERNAME')
-}.freeze
