c41c6d1b6746fff21c0bd7cd26766afd9c54087c
Stephen Murdoch <stephen@murdo.ch>
2018-10-04 15:11:27 +0100

    Admin controller and page with links to edit stuff

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   resource :about_page, only: [:show, :edit, :update], controller: 'about_page', path: 'about-mindful-life-therapy'
   resource :counselling_page, only: [:show, :edit, :update], controller: 'counselling_page', path: 'counselling-and-psychotherapy-in-glasgow'
   resource :mindfulness_page, only: [:show, :edit, :update], controller: 'mindfulness_page', path: 'mindfulness-in-glasgow'
-  resource :settings, only: [:show, :edit, :update]
+  resource :settings, only: [:edit, :update]
 
-  get '/contact', to: 'contact#new', as: 'contact'
-  post '/contact', to: 'contact#create', as: 'create_contact'
+  get '/contact-mindful-life-therapy', to: 'contact#new', as: 'contact'
+  post '/contact-mindful-life-therapy', to: 'contact#create', as: 'create_contact'
+  get '/admin', to: 'admin#show', as: 'admin'
 
   root 'home_page#show'
 end
