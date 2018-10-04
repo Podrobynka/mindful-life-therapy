b29542091f18827c13bc26d0c8bfe7e83890a1a0
Stephen Murdoch <stephen@murdo.ch>
2018-10-04 15:47:09 +0100

    Include site settings and 404 concerns in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :set_site_settings
-
-  private
-
-    def set_site_settings
-      @settings = Setting.first
-    end
+  include SiteSettings, Rescuer
 end
