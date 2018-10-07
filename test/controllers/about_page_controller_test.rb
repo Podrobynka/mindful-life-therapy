f04bed6e38aaafe1ec015d998362f8b87ee97d4c
Stephen Murdoch <stephen@murdo.ch>
2018-10-08 00:17:54 +0100

    User fixture_file_upload when attaching images in about_page controller test

--- test/controllers/about_page_controller_test.rb
+++ test/controllers/about_page_controller_test.rb
@@ -13,23 +13,23 @@ class AboutPageControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update about_page" do
-    patch about_page_url, params: { about_page: { body: about_pages(:one).body, page_photo: photo_fixture } }
+    patch about_page_url, params: { about_page: { body: about_pages(:one).body, page_photo: fixture_file_upload(photo_fixture, 'image/png') } }
     assert_redirected_to about_page_url
   end
 
   test "should update about_page via :xhr" do
-    patch about_page_url, xhr: true, params: { about_page: { body: about_pages(:one).body, page_photo: photo_fixture } }
+    patch about_page_url, xhr: true, params: { about_page: { body: about_pages(:one).body, page_photo: fixture_file_upload(photo_fixture, 'image/png') } }
     assert_match 'Turbolinks.visit("http://www.example.com/about-mindful-life-therapy", {"action":"replace"})', response.body
   end
 
   test "should show error message when update fails" do
-    patch about_page_url, params: { about_page: { body: '', page_photo: photo_fixture } }
+    patch about_page_url, params: { about_page: { body: '', page_photo: fixture_file_upload(photo_fixture, 'image/png') } }
     assert_match /1 error prohibited this record from being saved/, response.body
   end
 
   test "should show error message when update via :xhr fails" do
     page_photo = fixture_file_upload(photo_fixture, 'image/png')
-    patch about_page_url, xhr: true, params: { about_page: { body: '', page_photo: photo_fixture } }
+    patch about_page_url, xhr: true, params: { about_page: { body: '', page_photo: fixture_file_upload(photo_fixture, 'image/png') } }
     assert_match /1 error prohibited this record from being saved/, response.body
   end
 end
