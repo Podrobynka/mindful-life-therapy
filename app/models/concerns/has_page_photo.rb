16c16a12cf001b278100b91d69115de49b1bbefe
Stephen Murdoch <stephen@murdo.ch>
2018-10-07 16:25:20 +0100

    Start using custom validators for file size and content type

--- app/models/concerns/has_page_photo.rb
+++ app/models/concerns/has_page_photo.rb
@@ -3,29 +3,59 @@ module HasPagePhoto
 
   included do
     has_one_attached :page_photo
+    validates :page_photo, file_size: allowed_size, content_type: allowed_content_types
   end
 
   def display_photo
-    page_photo.attached? ? stored_photo : fallback_photo
+    page_photo.blob.present? && page_photo.attached? ? stored_photo : fallback_photo
   end
 
   def stored_photo
-    page_photo.variant(resize: photo_variant)
-  end
-
-  def photo_title
-    translation_for 'caption'
+    page_photo.variant(resize: variant_size)
   end
 
   def fallback_photo
     translation_for 'fallback'
   end
 
-  def photo_variant
+  def variant_size
     translation_for 'variant'
+  end
+
+  def photo_title
+    translation_for 'caption'
   end
 
   def translation_for attr
     I18n.t("page_photo.#{self.class.name.underscore}.#{attr}")
   end
+
+  class_methods do
+    def allowed_content_types
+      ['cun', 'to']
+      # ['image/png', 'image/jpg', 'image/jpeg', 'image/gif']
+    end
+
+    def allowed_size
+      10.megabytes
+    end
+  end
+
+  # def validate_page_photo
+  #   validate_page_photo_size_and_content_type if page_photo.attached?
+  # end
+
+  # def validate_page_photo_size_and_content_type
+  #   if page_photo.byte_size > 10.megabytes
+  #     purge_photo
+  #     errors.add :page_photo, 'is too big, the maximum filesize is 10MB.'
+  #   elsif !%w(image/png image/gif image/jpeg image/jpg).include?(page_photo.content_type)
+  #     purge_photo
+  #     errors.add :page_photo, 'is the wrong format, it must be png, gif or jpeg.'
+  #   end
+  # end
+  #
+  # def purge_photo
+  #   new_record? ? page_photo.purge : reload.page_photo.purge
+  # end
 end
