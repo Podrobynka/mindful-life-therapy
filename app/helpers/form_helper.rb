2230def08ffb2f31697ed3fc56573bfc03759e23
Stephen Murdoch <stephen@murdo.ch>
2018-10-09 01:24:39 +0100

    Helper methof for form title

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,4 +1,10 @@
 module FormHelper
+  def form_title_for record
+    content_tag :h1, class: 'text-center lmb lpb' do
+      t "forms.#{record.class.name.underscore}.title"
+    end
+  end
+
   def error_message target: 'errors.message'
     content_tag :div, nil, class: 'error-message hide', data: { target: target }
   end
