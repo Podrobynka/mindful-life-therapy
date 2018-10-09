cdaf7c1610d997554a7c4612f04e56b6bd97ada3
Stephen Murdoch <stephen@murdo.ch>
2018-10-09 19:52:01 +0100

    Test form_title_for helper method

--- test/helpers/form_helper_test.rb
+++ test/helpers/form_helper_test.rb
@@ -2,6 +2,12 @@ require 'test_helper'
 
 class FormHelperTest < ActionView::TestCase
 
+  test 'form_title_for' do
+    expected = "<h1 class='text-center lmb lpb'>Edit the About Page</h1>"
+    actual = form_title_for about_pages(:one)
+    assert_dom_equal expected, actual
+  end
+
   test 'error_message returns a div with css classes and a data attribute' do
     tag = "<div class='error-message hide' data-target='hello.boom'></div>"
     assert_dom_equal tag, error_message(target: 'hello.boom')
