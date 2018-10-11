37d98d2244a24d6258da996c76944550e88bf740
Stephen Murdoch <stephen@murdo.ch>
2018-10-11 04:41:40 +0100

    Testing the button_css and form_button_css helper methods

--- test/helpers/form_helper_test.rb
+++ test/helpers/form_helper_test.rb
@@ -2,8 +2,23 @@ require 'test_helper'
 
 class FormHelperTest < ActionView::TestCase
 
+  test 'button_css returns string and with default color if no argument is provided' do
+    expected = "button strong success dark-border-bottom nmb button-border-bottom button-text-shadow"
+    assert_equal expected, button_css
+  end
+
+  test 'button_css argument changes the default color' do
+    expected = "button strong primary dark-border-bottom nmb button-border-bottom button-text-shadow"
+    assert_equal expected, button_css(color: 'primary')
+  end
+
+  test 'form_button_css returns a string' do
+    expected = "button strong success dark-border-bottom nmb button-border-bottom button-text-shadow large"
+    assert_equal expected, form_button_css
+  end
+
   test 'form_title_for' do
-    expected = "<h1 class='text-center lmb lpb'>Edit the About Page</h1>"
+    expected = "<h1 class='text-center lmb'>Edit the About Page</h1>"
     actual = form_title_for about_pages(:one)
     assert_dom_equal expected, actual
   end
