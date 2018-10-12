b5239de1887f7936a613418bb8040a0d17b79bce
Stephen Murdoch <stephen@murdo.ch>
2018-10-12 05:45:35 +0100

    Import my map js file#

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,3 +29,6 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 const application = Application.start()
 const context = require.context("controllers", true, /.js$/)
 application.load(definitionsFromContext(context))
+
+import "../src/map"
+// const Trix = require("trix")
