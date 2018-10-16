c252accb47ec6d41c8e132b876e43560e5fe6f28
Stephen Murdoch <stephen@murdo.ch>
2018-10-16 15:24:56 +0100

    Change url for map page

--- test/controllers/map_controller_test.rb
+++ test/controllers/map_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class MapControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
-    get map_url
+    get map_page_url
     assert_response :success
   end
 end
