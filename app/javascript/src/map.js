eee3538b09e447825aa4763633d55a1eb8cb9bd0
Stephen Murdoch <stephen@murdo.ch>
2018-10-25 06:50:32 +0100

    Pull lat and long from data-latitude and data-longitude

--- app/javascript/src/map.js
+++ app/javascript/src/map.js
@@ -16,8 +16,9 @@ function initMap() {
   if (!mapDiv) return
 
   const map = L.map(mapDiv)
-  // const defaultCenter = [55.866437, -4.274812]
-  const defaultCenter = [55.866485912007, -4.2751044448164]
+  const latitude  = mapDiv.dataset.latitude  || 55.866437
+  const longitude = mapDiv.dataset.longitude || -4.274812
+  const defaultCenter = [latitude, longitude]
   const defaultZoom = 15
   const basemap = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
 	  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
