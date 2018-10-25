f4d04ee99c56dd0417db5539b7fc2417e741e5b1
Stephen Murdoch <stephen@murdo.ch>
2018-10-25 05:13:59 +0100

    Change geocode attrs

--- app/javascript/src/map.js
+++ app/javascript/src/map.js
@@ -16,7 +16,8 @@ function initMap() {
   if (!mapDiv) return
 
   const map = L.map(mapDiv)
-  const defaultCenter = [55.866437, -4.274812]
+  // const defaultCenter = [55.866437, -4.274812]
+  const defaultCenter = [55.866485912007, -4.2751044448164]
   const defaultZoom = 15
   const basemap = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
 	  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
