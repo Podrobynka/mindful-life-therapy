4d191ec3289462fc09f4b42e27edfad2ed8e8401
Stephen Murdoch <stephen@murdo.ch>
2018-10-25 01:53:42 +0100

    Geocode settings record in development and production environments

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,4 +1,6 @@
 class Setting < ApplicationRecord
+  geocoded_by :postcode
+
   validates :telephone, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 150 }, format: {
     with: URI::MailTo::EMAIL_REGEXP,
@@ -9,6 +11,12 @@ class Setting < ApplicationRecord
   validates :office_address_line_2, length: { maximum: 50 }
   validates :office_address_line_3, length: { maximum: 50 }
   validates :office_address_city, presence: true, length: { maximum: 50 }
-  validates :office_address_postcode, presence: true, length: { maximum: 50 }
+  validates :office_address_postcode, presence: true, length: { maximum: 50 }, postcode: {
+    if: ->(record) { record.office_address_postcode.present? }
+  }
   validates :session_rate, presence: true, length: { maximum: 50 }
+
+  unless Rails.env.test?
+    after_validation :geocode, if: ->(record) { record.saved_change_to_postcode? }
+  end
 end
