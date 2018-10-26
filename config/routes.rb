bd51965f57bea210fec17b7f6d806688db3169e9
Stephen Murdoch <stephen@murdo.ch>
2018-10-26 06:43:21 +0100

    Login, logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'login', to: 'logins#new', as: :login
+  get 'login/create', to: 'logins#create', as: :create_login
+  delete 'login', to: 'logins#destroy', as: :logout
+
   resource :about_page, only: [:show, :edit, :update], controller: 'about_page', path: 'about-mindful-life-therapy'
   resource :counselling_page, only: [:show, :edit, :update], controller: 'counselling_page', path: 'counselling-and-psychotherapy-in-glasgow'
   resource :mindfulness_page, only: [:show, :edit, :update], controller: 'mindfulness_page', path: 'mindfulness-training-in-glasgow'
