4228a4d6c2d9d0039bd73b0d044b5e658495d652
Stephen Murdoch <stephen@murdo.ch>
2018-10-27 00:59:38 +0100

    Test logout

--- test/controllers/logins_controller_test.rb
+++ test/controllers/logins_controller_test.rb
@@ -23,21 +23,22 @@ class LoginsControllerTest < ActionDispatch::IntegrationTest
     get create_login_url
     assert_redirected_to login_url
     assert_nil controller.current_user
-    assert_equal 'Authentication failed, please try again', controller.flash[:alert]
+    assert_equal 'Authentication failed, are you a registered member of the site?', controller.flash[:alert]
   end
 
-  # test "should get create" do
-  #   FakeResponse = Struct.new(:user_id)
-  #   GoogleSignIn::Identity.stubs(:new).returns FakeResponse.new('123456')
-  #   LoginsController.any_instance.stubs(:flash).returns({ google_sign_in_token: "abc123" })
-  #
-  #   VCR.use_cassette("login_with_gmail") do
-  #     assert_gets create_login_url
-  #   end
-  # end
+  test "destroy should log the user" do
+    GoogleSignIn::Identity.stubs(:new).returns OpenStruct.new(user_id: '123456')
+    LoginsController.any_instance.stubs(:flash).returns({ google_sign_in_token: "some token" })
 
-  # test "should logout" do
-  #   assert_gets create_login_url, google_login_params
-  # end
+    # log the user in
+    get create_login_url
+    assert_equal users(:one), controller.current_user
 
+    # log the user out
+    delete logout_url
+
+    assert_redirected_to root_url
+    assert_nil controller.current_user
+    assert_equal 'Successfully logged out', controller.flash[:notice]
+  end
 end
