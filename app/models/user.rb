37254018f7d6ca39699019c6c921417eb6ba39bc
Stephen Murdoch <stephen@murdo.ch>
2018-10-27 23:56:17 +0100

    Validate and test user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :google_id, presence: true, length: { maximum: 200 }
+  validates :email, presence: true, length: { maximum: 150 }, format: {
+    with: URI::MailTo::EMAIL_REGEXP,
+    message: 'is not a valid email address',
+    allow_blank: true
+  }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @user = users(:one)
+  end
+
+  test 'name' do
+    assert_required @user, :name
+    assert_too_long @user, :name, 101
+    assert_valid_length @user, :name, 100
+  end
+
+  test "contact_email" do
+    assert_required @user, :email
+    assert_email_too_long @user, 151
+    assert_email_valid_length @user, 150
+    assert_invalid_emails_are_rejected @user
+    assert_valid_emails_are_accepted @user
+    assert_blank_email_only_fails_presence_validation @user
+  end
+
+  test 'google_id' do
+    assert_required @user, :google_id
+    assert_too_long @user, :google_id, 201
+    assert_valid_length @user, :google_id, 200
+  end
 end
